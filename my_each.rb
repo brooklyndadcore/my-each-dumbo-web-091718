13f3b7b9d14c1a636cfd18a8266416cb7d61eeba
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-11 14:57:36 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,10 @@
 
-def my_each(array)
-  i = 0 
-  while i < array.length
-    yield array[i]
-    i += 1
-  end
-   return array
+def my_each
+   yield
 end
 
-my_each { |i| p i }
+my_each(array) do |i|
+  p i
+  return array
+end
 
